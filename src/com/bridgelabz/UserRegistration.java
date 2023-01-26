fc2c60debae03d650c857f383b3005ceecc54366
Your Name <you@example.com>
2023-01-26 14:11:15 +0530

    Added: First name starts with cap and has minimum 3 characters

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome To User Registration Problem.....");
+    public void validFirstName() {
+        System.out.print("Enter the First Name : ");
+        String name = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+
+        if (result)
+            System.out.println("First name is valid");
+        else
+            System.out.println("First name is Invalid");
     }
 }
---
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To User Registration Problem.....");
+
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.validFirstName();
+    }
+}
